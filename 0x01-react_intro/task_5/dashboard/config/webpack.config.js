00ecd40e5ebe2130d748d665da315a7a765c6f7c
manuel4life <allotey4eva@gmail.com>
2023-09-30 00:40:50 +0200

    .\task_5\

---
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    devServer: {
+        static: path.join(__dirname, '../dist'),
+        hot: true,
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: 'file-loader',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'], // Add .jsx as an extension to resolve
+    },
+};
